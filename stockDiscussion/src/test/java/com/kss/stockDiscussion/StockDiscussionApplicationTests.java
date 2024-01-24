2f1d3e01c8d45a2f16542ecaa9d61c02227c9525
KoKimss <seungsu8848@ajou.ac.kr>
2024-01-24 12:14:30 +0900

    first commit

---
+++ stockDiscussion/src/test/java/com/kss/stockDiscussion/StockDiscussionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kss.stockDiscussion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDiscussionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
